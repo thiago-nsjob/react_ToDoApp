cc2fef6af4bddb198a10e26d42dcea97bdbc6630
thiago-nsjob <thiago.nsjob@gmail.com>
2018-05-21 07:53:43 -0400

    projct structure

---
+++ kambam.App/webpack.config.js
@@ -0,0 +1,49 @@
+/*
+    todo: index.html has not been copy to dist
+          
+*/
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin();
+const config = {
+    plugins: [
+      new CopyWebpackPlugin([{ from: 'src/index.html', to: 'dest', toType: 'file'}])
+    ]
+  }
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+         new HtmlWebpackPlugin({
+          template:'./src/index.html'
+        })
+      ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+    },
+  mode:"none",
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  }
+}
